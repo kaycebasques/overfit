fba5095e591acb1ae39f662decc2870a4a604717
Kayce Basques <kayce@google.com>
2024-03-02 15:46:38 -0800

    feat: change theme

--- conf.py
+++ conf.py
@@ -2,7 +2,7 @@ project = 'overfit.dev'
 copyright = '2024, Kayce Basques'
 author = 'Kayce Basques'
 release = '0.0.0'
-extensions = ['sphinx-pigweed']
+extensions = []
 exclude_patterns = [
     '.github',
     '.gitignore',
@@ -15,7 +15,6 @@ exclude_patterns = [
     'venv'
 ]
 pygments_style = 'sphinx'
-html_theme = 'sphinx-pigweed'
+html_theme = 'traditional'
 # html_static_path = ['_static']
 html_permalinks_icon = '#'
-# pw_banner_text = 'Hello, world!'
