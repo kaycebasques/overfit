da13666c294a78b107c97c61d4c71bf0f24a1fee
Kayce Basques <kayce@google.com>
2024-12-29 19:43:51 -0800

    Don't fail on warnings

--- build.fish
+++ build.fish
@@ -2,5 +2,5 @@ if not test -d venv; python3 -m venv venv; end
 . venv/bin/activate.fish
 python3 -m pip install -r requirements.txt
 if test -d _build; rm -rf _build; end
-sphinx-build -M html . _build --fail-on-warning
+sphinx-build -M html . _build
 deactivate
